93b88835470dd71a34c35dfaa4dcb1c553c53ec3
AdamFixed <adamnr012@gmail.com>
2025-11-03 12:52:10 +0100

    Añadido Sysos a Hola.Java

--- src/miproyecto/Hola.java
+++ src/miproyecto/Hola.java
@@ -3,7 +3,8 @@ package miproyecto;
 public class Hola {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo!");
+		System.out.println("Hola Git!");
 
 	}
 
